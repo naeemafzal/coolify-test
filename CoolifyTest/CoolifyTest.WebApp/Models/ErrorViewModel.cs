85613a06602fccc6ac9487bb7d34d9ef9650b922
Naeem Afzal <naeem4216@yahoo.com>
2025-06-28 21:46:28 +0100

    Adding test app

---
+++ CoolifyTest/CoolifyTest.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoolifyTest.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
