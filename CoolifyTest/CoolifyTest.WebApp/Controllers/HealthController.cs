b90bcca586afed5f885632080ce5c96e97749ffd
Naeem Afzal <naeem4216@yahoo.com>
2025-06-28 21:57:06 +0100

    adding health check

---
+++ CoolifyTest/CoolifyTest.WebApp/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoolifyTest.WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
